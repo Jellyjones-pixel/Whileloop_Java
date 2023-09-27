76e417cbe9a0682931ead5f9d0c00c9aab556d16
JOHN MBITHI MUTAVE <83606270+Jellyjones-pixel@users.noreply.github.com>
2023-09-27 16:30:14 +0100

    Add files via upload
    Updated

---
+++ WhileLoopExample1/src/whileloopexample1/WhileLoopExample1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package whileloopexample1;
+
+/**
+ *
+ * @author jelly_joneske
+ */
+public class WhileLoopExample1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                
+        int i=1;
+        while(i<=10){
+            System.out.println("Iteration Number" );
+            i++;}
+    }
+    
+}
